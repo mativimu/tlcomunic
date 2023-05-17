9942c5c979883e44fea03ced55f47d0c991ae264
mativimu <mativimu.u@gmail.com>
2023-05-17 00:23:53 -0400

    AUT project setted up #1

---
+++ core/apps/aut/src/main/java/com/tlcomunic/aut/AutApplication.java
@@ -0,0 +1,13 @@
+package com.tlcomunic.aut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutApplication.class, args);
+	}
+
+}
