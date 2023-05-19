7a0aa62fe60ef67266694edb0fb916d62cd168e0
mativimu <mativimu.u@gmail.com>
2023-05-18 21:47:07 -0400

    Role and User classes created #1

---
+++ core/apps/aut/src/main/java/com/tlcomunic/aut/domain/Role.java
@@ -0,0 +1,7 @@
+package com.tlcomunic.aut.domain;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ core/apps/aut/src/main/java/com/tlcomunic/aut/domain/User.java
@@ -0,0 +1,102 @@
+package com.tlcomunic.aut.domain;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@Entity
+@AllArgsConstructor
+@Table(name="tlcomunic_user")
+public class User implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+        name = "uid_sequence",
+        sequenceName = "uid_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "uid_sequence")
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 25)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 25)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 64)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "enable", nullable = false)
+    private boolean enable = true;
+
+    @Column(name = "account_non_expired", nullable = false)
+    private boolean accountNonExpired = true;
+
+    @Column(name = "account_non_locked", nullable = false)
+    private boolean accountNonLocked = true;
+
+    @Column(name = "credentials_non_expired", nullable = false)
+    private boolean credentialsNonExpired = true;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
+    }
+
+
+}
